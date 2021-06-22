739b698e4e048ee5cf486f74282d3e50bb7b17f5
Albin Groen <albin.groen@gmail.com>
2021-06-22 16:32:30 +0200

    :speech_balloon: Fix text issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
       <p className="text-sm leading-relaxed text-gray-400 dark:text-gray-500">
         Weblists is an Open Source project with the goal to build a platform
         where people can share lists with anyone on the internet, without having
-        to sign up anywhere. It's a project built by{" "}
+        to sign up anywhere. It&apos;s a project built by{" "}
         <a className="link" href="https://albingroen.com">
           Albin Groen
         </a>
