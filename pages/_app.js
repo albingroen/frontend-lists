7a826388a7e9ed5a0aca60e2ccf35120400881d2
Albin Groen <albin.groen@gmail.com>
2021-06-22 16:52:56 +0200

    :chart_with_upwards_trend: Add Splitbee

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import { Toaster } from "react-hot-toast";
 import "../styles/globals.css";
+import splitbee from "@splitbee/web";
+
+// This initiliazes Splitbee.js
+splitbee.init();
 
 function MyApp({ Component, pageProps }) {
   return (
