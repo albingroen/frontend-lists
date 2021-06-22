04ca90df9715dcdf3bd7386414c8c4e8075e22e7
Albin Groen <albin.groen@gmail.com>
2021-06-22 14:37:23 +0200

    :wrench: Update prod url

--- lib/config.ts
+++ lib/config.ts
@@ -3,7 +3,7 @@ export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 export const appUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://frontend-lists.vercel.app";
+    : "https://www.weblists.io";
 
 export const siteInfo = {
   title: "Weblists | Create text lists and share them with anyone",
